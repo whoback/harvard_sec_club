d78580a73ba8328861f4778c68528b314ea7e707
Will Hoback <will.hoback@gmail.com>
2020-09-29 22:42:06 -0400

    regex for  parsing sites

--- sh.sh
+++ sh.sh
@@ -1,5 +1,5 @@
 # grab top urls from alexa.com
  curl -o alexa.txt https://www.alexa.com/topsites
 # grep for website
-TODO
+# cat alexa.txt | grep something like (?<=\/siteinfo\/).*?(?=")
 # need to Write a bash script that takes a file of hostnames as input and resolves them to IP addresses using the 'host' or 'nslookup' commands
