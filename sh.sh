ce6b7de36c31f02253ee526ab03d2e3fe269fd15
Will Hoback <will.hoback@gmail.com>
2020-09-30 11:06:34 -0400

    curl for sites and store to array

--- sh.sh
+++ sh.sh
@@ -1,5 +1,17 @@
+#!/usr/bin/env bash
+
+# check for passed in file
+
+# if no file get urls from alexa.com
+
 # grab top urls from alexa.com
- curl -o alexa.txt https://www.alexa.com/topsites
+
+
+targets=($( curl -s https://www.alexa.com/topsites | grep -oP '(?<=\/siteinfo\/).*?(?=")'))
+length=${#targets[@]}
+for ((i = 0; i != length; i++)); do
+   echo "target $i: '${targets[i]}'"
+done
 # grep for website
 # cat alexa.txt | grep something like (?<=\/siteinfo\/).*?(?=")
 # need to Write a bash script that takes a file of hostnames as input and resolves them to IP addresses using the 'host' or 'nslookup' commands
