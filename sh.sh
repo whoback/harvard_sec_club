3dd45865f423d07fc4f89ed606eb7cd601432ef2
Will Hoback <will.hoback@gmail.com>
2020-09-30 11:54:21 -0400

    change testing file and get cmd arg working

--- sh.sh
+++ sh.sh
@@ -1,17 +1,39 @@
-#!/usr/bin/env bash
+#!/bin/bash
+# saner programming env: these switches turn some bugs into errors
+set -o errexit -o pipefail -o noclobber -o nounset
 
-# check for passed in file
+function get_top_50_websites {
+    targets=($( curl -s https://www.alexa.com/topsites | grep -oP '(?<=\/siteinfo\/).*?(?=")'))
+    length=${#targets[@]}
+    for ((i = 0; i != length; i++)); do
+        echo "$i: '${targets[i]}'"
+        host ${targets[i]}
+    done
+}
 
-# if no file get urls from alexa.com
+USAGE="Usage: $0 -f FILE or -g to generate hostname info for top 50 websites"
 
-# grab top urls from alexa.com
+if [ "$#" == "0" ]; then
+	echo "$USAGE"
+	exit 1
+fi
 
-
-targets=($( curl -s https://www.alexa.com/topsites | grep -oP '(?<=\/siteinfo\/).*?(?=")'))
-length=${#targets[@]}
-for ((i = 0; i != length; i++)); do
-   echo "target $i: '${targets[i]}'"
+while getopts ":f:" opt; do
+  case ${opt} in
+    f )
+      tar=$OPTARG
+      cat $tar | while read line; do host $line; done
+      ;;
+    \? )
+      echo "Invalid option: $OPTARG" 1>&2
+      ;;
+    : )
+      echo "Invalid option: $OPTARG requires an argument" 1>&2
+      ;;
+  esac
 done
+shift $((OPTIND -1))
+
 # grep for website
 # cat alexa.txt | grep something like (?<=\/siteinfo\/).*?(?=")
 # need to Write a bash script that takes a file of hostnames as input and resolves them to IP addresses using the 'host' or 'nslookup' commands
